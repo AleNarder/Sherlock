900f827e3d9efeeb90fc4dd8ee17260bc7a2a5cf
Alessio <alessio.narder@kaigos.io>
2024-07-25 22:04:47 +0000

    refacto(scanner): removed tool scanner

--- ros2/src/tool_scanner/tool_scanner/controller.py
+++
@@ -1,9 +0,0 @@
-class RobotController:
-    def __init__(self, robot):
-        self.robot = robot
-
-    def move(self, x, y, z):
-        self.robot.move(x, y, z)
-
-    def scan(self):
-        self.robot.scan()
--- ros2/src/tool_scanner/tool_scanner/scanner.py
+++
@@ -1,3 +0,0 @@
-class Scanner ():
-    def __init__(self, recorder, controller) -> None:
-        pass
