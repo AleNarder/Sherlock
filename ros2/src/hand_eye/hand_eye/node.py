00efb21908dcb20e61f5c450d1b2f4fa753c9383
Alessio <alessio.narder@kaigos.io>
2024-08-02 23:57:28 +0000

    feat(had-eye): working hand-eye

--- ros2/src/hand_eye/hand_eye/node.py
+++ ros2/src/hand_eye/hand_eye/node.py
@@ -5,10 +5,13 @@ import transforms3d
 import json
 import os
 import cv2
+import copy
+import time
 
+from rclpy.executors import MultiThreadedExecutor
 from realsense2_camera_msgs.msg import Extrinsics
 from intrinsics.utils import is_sharp, get_chessboard_pts, compute_reprojection_error
-from hand_eye.utils import compute_hand_eye, find_chessboard_corners
+from hand_eye.utils import compute_hand_eye, homogenous_matrix_from_rt, find_chessboard_corners, P_cam2ros
 from statemachine import StateMachine, State
 from rclpy.node import Node
 from tf_transformations import quaternion_matrix
@@ -29,10 +32,13 @@ class HandEyeNode (StateMachine, Node):
     INITIALIZED = State(name="INITIALIZED") 
     RECORDING   = State(name="RECORDING")
     PROCESSING  = State(name="PROCESSING")
+
     
     initialize      = UNITIALIZED.to(INITIALIZED, cond = "intrinsics_are_loaded") | UNITIALIZED.to(UNITIALIZED)
+    
     start_recording = INITIALIZED.to(RECORDING) 
     stop_recording  = RECORDING.to(PROCESSING)
+
     processing_done = PROCESSING.to(INITIALIZED)
     
     def __init__ (self, base_frame_id, hand_frame_id, eye_frame_id):        
@@ -62,7 +68,7 @@ class HandEyeNode (StateMachine, Node):
         self.create_timer(0.1, self._publish)
     
         self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self, spin_thread=True)
         self.br = TransformBroadcaster(self)
 
     def on_initialize (self):
@@ -74,7 +80,7 @@ class HandEyeNode (StateMachine, Node):
             self.get_logger().info("hand eye tf loaded!")
         
     def after_initialize(self):
-        self.create_subscription(TFMessage, "/tf", self._on_tf, 10)
+        self.create_subscription(TFMessage, "/tf", self._on_tf, 10, )
             
     def intrinsics_are_loaded(self):
         if self.intrinsics["mtx"] is None or self.intrinsics["dist"] is None:
@@ -117,21 +123,20 @@ class HandEyeNode (StateMachine, Node):
     def _on_image_cb(self, msg: Image):
         if self.current_state == self.RECORDING:
             cv_frame  = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-            frame_is_sharp = is_sharp(cv_frame, 150)
+            frame_is_sharp = is_sharp(cv_frame, 250)
             
             if not frame_is_sharp:
                 self.get_logger().info("Image is not sharp enough")
                 return
             
-            timestap = msg.header.stamp
-            pose = self.get_homogeneous_matrix(timestap)
+            timestamp = msg.header.stamp
+            pose, pose_tf = self.get_homogeneous_matrix(timestamp)
                 
             if pose is None:
                 self.get_logger().info("Unable to recover consistent pose for current image")
                 return
             
-            self.frames.append(cv_frame)          
-            self.poses.append(pose)
+            self.get_logger().info(f"time delta is: 0.{pose_tf.header.stamp.nanosec - timestamp.nanosec}")
             
             chessboard_pts = get_chessboard_pts((9, 6), square_size=0.023)
             ret, corners = find_chessboard_corners(cv_frame, (9,6))
@@ -156,8 +161,16 @@ class HandEyeNode (StateMachine, Node):
                 self.intrinsics["dist"]
             )
             
-            self.get_logger().info("Chessboard proj err: " + str(err))
+            if err > 0.1:
+                self.get_logger().info("Chessboard proj err too high")
+                return
+            else:
+                self.get_logger().info("Chessboard proj err: " + str(err))
             
+            self.frames.append(cv_frame)          
+            self.poses.append(pose)
+            
+            # Publish tf for debug
             rot, _ = cv2.Rodrigues(rvec)
 
             quaternion  = transforms3d.quaternions.mat2quat(rot)
@@ -165,8 +178,8 @@ class HandEyeNode (StateMachine, Node):
             
             t = TransformStamped()
             # Set the time and frame IDs
-            t.header.stamp = msg.header.stamp
-            t.header.frame_id = "rgb_camera"
+            t.header.stamp = timestamp
+            t.header.frame_id = self.eye_frame_id
             t.child_frame_id  = "chessboard"
 
             # Set the translation
@@ -179,12 +192,13 @@ class HandEyeNode (StateMachine, Node):
             t.transform.rotation.x = quaternion[1]
             t.transform.rotation.y = quaternion[2]
             t.transform.rotation.z = quaternion[3]
-            
             self.br.sendTransform(t)
+            
 
     def _on_tf (self, msg: TFMessage):
-        stamp = msg.transforms[0].header.stamp
-        self._broadcast_tf(stamp)
+        if self.current_state != self.PROCESSING:
+            stamp = msg.transforms[0].header.stamp
+            self._broadcast_tf(stamp)
         
     def _on_intrinsics_cb(self, msg: String):
         data = json.loads(msg.data)
@@ -208,31 +222,42 @@ class HandEyeNode (StateMachine, Node):
 
     def get_homogeneous_matrix(self, stamp):
         try:
-            trans = self.tf_buffer.lookup_transform(self.base_frame_id, self.hand_frame_id_, stamp)
+            gripper2base = self.tf_buffer.lookup_transform(self.base_frame_id, self.hand_frame_id_, stamp)
+            
             translation = [
-                trans.transform.translation.x,
-                trans.transform.translation.y,
-                trans.transform.translation.z
+                gripper2base.transform.translation.x,
+                gripper2base.transform.translation.y,
+                gripper2base.transform.translation.z
             ]
+            
             rotation = [
-                trans.transform.rotation.w,
-                trans.transform.rotation.x,
-                trans.transform.rotation.y,
-                trans.transform.rotation.z
+                gripper2base.transform.rotation.w,
+                gripper2base.transform.rotation.x,
+                gripper2base.transform.rotation.y,
+                gripper2base.transform.rotation.z
             ]
-            T = quaternion_matrix(rotation)
-            T[0:3, 3] = translation
-            return T
+            
+            R = transforms3d.quaternions.quat2mat(rotation)
+            t = np.array(translation)
+
+            h = homogenous_matrix_from_rt(R, t)
+            
+            self.get_logger().info(f"Transform lookup successful!")
+            
+            return h, gripper2base
         except Exception as e:
             self.get_logger().error(f"Transform lookup failed: {e}")
-            return None  
+            return None, None
         
     def _get_hand_eye_tf (self, timestamp):    
         
         t = TransformStamped()
         
-        quaternion  = transforms3d.quaternions.mat2quat(self.hand_eye_h[:3, :3])
-        translation = self.hand_eye_h[:3, 3].reshape(3)
+        rot   = self.hand_eye_h[:3, :3]
+        trans = self.hand_eye_h[:3, 3] 
+        
+        quaternion  = transforms3d.quaternions.mat2quat(rot)
+        translation = trans.reshape(3)
         
         # Set the time and frame IDs
         t.header.stamp = timestamp
@@ -261,26 +286,22 @@ class HandEyeNode (StateMachine, Node):
                 self.br.sendTransform(tf)
     
     def _on_save (self):
-        np.save(os.path.join("/", "calibration", "data", "hand_eye", "h.npy"), self.hand_eye_h)
-        np.save(os.path.join("/", "calibration", "data", "hand_eye", "poses.npy"), self.poses)
+        np.save(os.path.join("/", "calibration", "data", "hand_eye", "h.npy"),      self.hand_eye_h)
+        np.save(os.path.join("/", "calibration", "data", "hand_eye", "poses.npy"),  self.poses)
         np.save(os.path.join("/", "calibration", "data", "hand_eye", "frames.npy"), self.frames)
 
     def _process(self):
         self.get_logger().info("processing...")
         
-        R_cam2gripper, t_cam2gripper, R_target2cam_r, t_target2cam_r = compute_hand_eye(
-            self.frames,
-            self.poses,
-            self.intrinsics
+        he, hs_gripper2base, hs_target2cam = compute_hand_eye(
+            self.frames, 
+            self.poses, 
+            self.intrinsics, 
+            self.get_logger()
         )
+
         
-        # !IMPORTANT: The hand-eye calibration is computed in the camera frame
-        #             but we need to express it in the robot base frame
-        
-        h = np.eye(4)
-        h[:3, :3] = R_cam2gripper
-        h[:3, 3]  = t_cam2gripper.reshape(3)
-        self.hand_eye_h = h
+        self.hand_eye_h = he
         self.processing_done()
         
 def main ():
@@ -290,7 +311,15 @@ def main ():
         hand_frame_id = "tcp",
         eye_frame_id  = "rgb_camera"
     )
-    rclpy.spin(node)
+    
+    # Create a MultiThreadedExecutor with the number of threads you want
+    executor = MultiThreadedExecutor(num_threads=4)
+
+    # Add the node to the executor
+    executor.add_node(node)
+    # Spin the executor (this will block until shutdown)
+    executor.spin()
+    
     rclpy.shutdown()
 
 
--- ros2/src/hand_eye/hand_eye/utils.py
+++ ros2/src/hand_eye/hand_eye/utils.py
@@ -1,7 +1,32 @@
 import numpy as np
 import cv2
 import cv2.typing as cv2_t
+from datetime import datetime, timedelta
+from builtin_interfaces.msg import Time as RosTime
 from intrinsics.utils import find_chessboard_corners, get_chessboard_pts, get_n_representative_frames, IntrinsicsDict
+
+P_cam2ros = np.array([
+    [0, 0, 1],
+    [-1, 0, 0],
+    [0, -1, 0]
+])
+
+def subtract_milliseconds(ros_timestamp, milliseconds):
+    # Convert ROS2 Time to datetime
+    seconds = ros_timestamp.sec
+    nanoseconds = ros_timestamp.nanosec
+    timestamp = datetime.fromtimestamp(seconds + nanoseconds / 1e9)
+    
+    # Subtract the specified milliseconds
+    new_timestamp = timestamp - timedelta(milliseconds=milliseconds)
+    
+    # Convert back to ROS2 Time
+    new_seconds = int(new_timestamp.timestamp())
+    new_nanoseconds = int((new_timestamp.timestamp() - new_seconds) * 1e9)
+    
+    new_ros_timestamp = RosTime(sec=new_seconds, nanosec=new_nanoseconds)
+    return new_ros_timestamp
+
 
 def homogenous_matrix_from_rt (r: np.array, t: np.array):
     h = np.eye(4)
@@ -9,7 +34,7 @@ def homogenous_matrix_from_rt (r: np.array, t: np.array):
     h[:3, 3] = t.reshape(3)
     return h
 
-def compute_hand_eye(frames: list[cv2_t.MatLike], poses: list[np.array], cam_data: IntrinsicsDict):
+def compute_hand_eye(frames: list[cv2_t.MatLike], poses: list[np.array], cam_data: IntrinsicsDict, logger):
         chessboard_pts = get_chessboard_pts((9, 6), square_size=0.023)
         
         frames = np.array(frames)
@@ -21,30 +46,41 @@ def compute_hand_eye(frames: list[cv2_t.MatLike], poses: list[np.array], cam_dat
         t_target2cam_r   = [] 
 
         # Only process sufficiently different frames to reduce computation time and avoid overfitting
-        representative_frames_idxs = get_n_representative_frames(frames, num_frames=75)
-        representative_frames, representative_poses  = frames[representative_frames_idxs], poses[representative_frames_idxs]
+        # representative_frames_idxs = get_n_representative_frames(frames, num_frames=75)
+        # representative_frames, representative_poses  = frames[representative_frames_idxs], poses[representative_frames_idxs]
         
-        for frame, pose in zip(representative_frames, representative_poses):
+        for frame, pose in zip(frames, poses):
             ret, corners = find_chessboard_corners(frame, (9,6))    
             if ret:
-                ret2, rvec, tvec = cv2.solvePnP(chessboard_pts, corners, cam_data["mtx"], cam_data["dist"])
+                ret2, rvec_target2cam, tvec_target2cam = cv2.solvePnP(chessboard_pts, corners, cam_data["mtx"], cam_data["dist"])
                 if ret2:
-                    R_gripper2base_r.append(pose[:3, :3])
-                    t_gripper2base_r.append(pose[:3, 3].reshape((3, 1)))
-                    
-                    rot, _ = cv2.Rodrigues(rvec)
-                    
-                    R_target2cam_r.append(rot)
-                    t_target2cam_r.append(tvec)
 
-                    
+                    rot_target2camm, _ = cv2.Rodrigues(rvec_target2cam)
+
+                    R_target2cam_r.append(rot_target2camm)
+                    t_target2cam_r.append(tvec_target2cam)
+                                        
+                    R_gripper2base_r.append(pose[:3, :3])
+                    t_gripper2base_r.append((pose[:3, 3]).reshape((3, 1)))
+
+
         R_cam2gripper, t_cam2gripper = cv2.calibrateHandEye(
             R_gripper2base_r, 
-            t_gripper2base_r, 
+            t_gripper2base_r,
+             
             R_target2cam_r, 
             t_target2cam_r, 
             method = cv2.CALIB_HAND_EYE_TSAI
         )
         
+    
+        h_cam2gripper   = homogenous_matrix_from_rt(R_cam2gripper, t_cam2gripper)
+        hs_gripper2base = [homogenous_matrix_from_rt(R, t) for R, t in zip(R_gripper2base_r, t_gripper2base_r)]
+        hs_target2cam   = [homogenous_matrix_from_rt(R, t) for R, t in zip(R_target2cam_r, t_target2cam_r)]
         
-        return R_cam2gripper, t_cam2gripper, R_target2cam_r, t_target2cam_r
+        return (
+            h_cam2gripper,
+            hs_gripper2base,
+            hs_target2cam
+        ) 
+            
